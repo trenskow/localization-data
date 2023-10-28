8d5dd31173b562b5767562c4fb1f113ae0b5c07f
Kristian Trenskow <trenskow@me.com>
2023-10-28 10:43:06 +0200

    Fixed breaking changes from `countries-list`.

--- index.js
+++ index.js
@@ -6,16 +6,9 @@ const
 
 const
 	scripts = require('./dist/scripts.json'),
-	languagesAllScripts = require('./dist/languages-scripts.json');
+	languagesScripts = require('./dist/languages-scripts.json');
 
-const languagesScripts = Object.keys(languagesAllScripts).reduce((res, language) => {
-	if (!countriesList.languages[language]) return res;
-	res[language] = languagesAllScripts[language];
-	return res;
-}, {});
-
-module.exports = merge(countriesList, {
+module.exports = merge({}, countriesList, {
 	scripts: scripts,
-	languages: merge.recursive(countriesList.languages, languagesScripts),
-	languagesAll: merge.recursive(countriesList.languagesAll, languagesAllScripts)
+	languages: merge.recursive(countriesList.languages, languagesScripts)
 });
