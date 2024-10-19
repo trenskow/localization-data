f5693c72df900f07a4dc1cf836673595bd4ea41d
Kristian Trenskow <trenskow@me.com>
2024-10-19 21:54:46 +0200

    Replaced `merge`.

--- index.js
+++ index.js
@@ -2,12 +2,12 @@ import { readFileSync } from 'fs';
 import { join } from 'path';
 
 import { countries, languages } from 'countries-list';
-import merge from 'merge';
+import merge from '@trenskow/merge';
 
 const scripts = JSON.parse(readFileSync(join(import.meta.dirname, 'dist', 'scripts.json'), 'utf8'));
 const languagesScripts = JSON.parse(readFileSync(join(import.meta.dirname, 'dist', 'languages-scripts.json'), 'utf8'));
 
 export default merge({}, countries, {
 	scripts: scripts,
-	languages: merge.recursive(languages, languagesScripts)
+	languages: merge(languages, languagesScripts)
 });
